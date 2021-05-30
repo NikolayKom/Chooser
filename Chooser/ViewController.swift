7237ef567d2f4b346ae94f1920ead871f141c517
Nikolay <k70456523@gmail.com>
2021-05-30 22:40:23 +0700

    Simple Yes or No chooser

---
+++ Chooser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleWatchApplication
+//
+//  Created by Николай on 30.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
